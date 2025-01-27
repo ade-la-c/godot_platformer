8a748ba9cfb55fd4d05ad5dd9bc939979001b6b8
ade-la-c <gringodlc@gmail.com>
2025-01-27 03:37:51 +0100

    level menu wip

--- Scripts/LevelMenu.cs
+++ Scripts/LevelMenu.cs
@@ -5,6 +5,7 @@ using System.Xml.Schema;
 public partial class LevelMenu : Control {
 
 	[Export] PackedScene[] levels;
+	public VBoxContainer levelButtonContainer;
 	private int currentLevelIndex = 0;
 
 	// Called when the node enters the scene tree for the first time.
@@ -13,6 +14,7 @@ public partial class LevelMenu : Control {
 		GenerateLevelButtons();
 
 		GetNode<Button>("VBoxContainer/BackButton").GrabFocus();
+		levelButtonContainer = GetNode<VBoxContainer>("VBoxContainer/LevelButtons");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,6 +24,12 @@ public partial class LevelMenu : Control {
 
 		// TODO generate buttons based on levels array
 
+		for (int i = levels.Length; i > 0; i--) {
+			var scene = GD.Load<PackedScene>("res://Scenes/UI/LevelButton.tscn");
+			var inst = scene.Instantiate<Button>();
+			levelButtonContainer.AddChild(inst);
+			// AddChild(inst);
+		}
 	}
 
 	public void Next() {
